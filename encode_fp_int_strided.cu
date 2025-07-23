25ea79216f95a54171279fb35d1fb68213eebb14
Qianxiang Ma <qianxiang.ma@case.edu>
2025-07-23 17:01:31 +0900

    fixes for complex

--- encode_fp_int_strided.cu
+++ encode_fp_int_strided.cu
@@ -55,7 +55,7 @@ struct uint_transpose {
 
 template <class real_t, class real_const_ptr, int32_t GRID_X, int32_t GRID_Y, int32_t BLOCK_THREADS, int32_t COMPLEX, int32_t ORDER>
 __global__ void encode_fp_strided_i8(int32_t order, int32_t M, int32_t N, real_const_ptr A, int32_t lda, const int32_t* __restrict__ vec_expon, uint32_t* __restrict__ inA, int32_t ldi, int32_t stride) {
-  constexpr int32_t items = 4 << COMPLEX;
+  constexpr int32_t items = 1 << COMPLEX;
   constexpr int32_t elements = items * BLOCK_THREADS * GRID_Y;
   constexpr int32_t elements_block = items * BLOCK_THREADS;
 
@@ -75,7 +75,7 @@ __global__ void encode_fp_strided_i8(int32_t order, int32_t M, int32_t N, real_c
 
     for (int32_t i = blockIdx.x * elements_block; i < M; i += elements) {
       int32_t num_items = min(elements_block, M - i);
-      uint32_t* __restrict__ inA_i = &inA[(i >> 2) + col * ldi];
+      uint32_t* __restrict__ inA_i = &inA[(i >> COMPLEX) + col * ldi];
       block_load.Load(&A_i[i], threadA, num_items, real_t());
 
       #pragma unroll
@@ -92,7 +92,7 @@ __global__ void encode_fp_strided_i8(int32_t order, int32_t M, int32_t N, real_c
       for (int32_t k = 0; k < ORDER; ++k)
         trans_f(*reinterpret_cast<uint32_t(*)[items]>(&threadB[k * items]));
       
-      num_items = (num_items + 3) >> 2;
+      num_items = (num_items + items - 1) >> COMPLEX;
       for (int32_t k = 0; k < order; ++k)
         block_store.Store(&inA_i[k * stride], *reinterpret_cast<uint32_t(*)[1]>(&threadB[k]), num_items);
     }
@@ -105,21 +105,21 @@ constexpr int32_t grid_y = 64;
 constexpr int32_t order_max = 5;
 
 void internal::int8::encode_f64_order20(cudaStream_t stream, int32_t order, int32_t M, int32_t N, const double* A, int32_t lda, const int32_t* vec_expon, int8_t* inA, int32_t ldi, int32_t stride) {
-  encode_fp_strided_i8 <double, const double* __restrict__, grid_x, grid_y, block_threads, 0, order_max>
+  encode_fp_strided_i8 <double, const double* __restrict__, grid_x, grid_y, block_threads, 2, order_max>
   <<< dim3(grid_y, grid_x, 1), block_threads, 0, stream >>> (order, M, N, (const double*)A, lda, vec_expon, (uint32_t*)inA, ldi >> 2, stride >> 2);
 }
 
 void internal::int8::encode_f32_order20(cudaStream_t stream, int32_t order, int32_t M, int32_t N, const float* A, int32_t lda, const int32_t* vec_expon, int8_t* inA, int32_t ldi, int32_t stride) {
-  encode_fp_strided_i8 <float, const float* __restrict__, grid_x, grid_y, block_threads, 0, order_max>
+  encode_fp_strided_i8 <float, const float* __restrict__, grid_x, grid_y, block_threads, 2, order_max>
   <<< dim3(grid_y, grid_x, 1), block_threads, 0, stream >>> (order, M, N, (const float*)A, lda, vec_expon, (uint32_t*)inA, ldi >> 2, stride >> 2);
 }
 
 void internal::int8::encode_cf64_order20(cudaStream_t stream, int32_t order, int32_t M, int32_t N, const std::complex<double>* A, int32_t lda, const int32_t* vec_expon, int8_t* inA, int32_t ldi, int32_t stride) {
-  encode_fp_strided_i8 <double, const double* __restrict__, grid_x, grid_y, block_threads, 1, order_max>
+  encode_fp_strided_i8 <double, const double* __restrict__, grid_x, grid_y, block_threads, 3, order_max>
   <<< dim3(grid_y, grid_x, 1), block_threads, 0, stream >>> (order * 2, M * 2, N, (const double*)A, lda * 2, vec_expon, (uint32_t*)inA, ldi >> 2, stride >> 2);
 }
 
 void internal::int8::encode_cf32_order20(cudaStream_t stream, int32_t order, int32_t M, int32_t N, const std::complex<float>* A, int32_t lda, const int32_t* vec_expon, int8_t* inA, int32_t ldi, int32_t stride) {
-  encode_fp_strided_i8 <float, const float* __restrict__, grid_x, grid_y, block_threads, 1, order_max>
+  encode_fp_strided_i8 <float, const float* __restrict__, grid_x, grid_y, block_threads, 3, order_max>
   <<< dim3(grid_y, grid_x, 1), block_threads, 0, stream >>> (order * 2, M * 2, N, (const float*)A, lda * 2, vec_expon, (uint32_t*)inA, ldi >> 2, stride >> 2);
 }
